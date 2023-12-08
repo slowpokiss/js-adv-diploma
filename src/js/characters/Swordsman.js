ca04045932d54944790ace29a325ab8e1eccc89e
Michel <hltvexe@mail.ru>
2023-12-08 20:06:13 +0300

    finale 1.0.0

--- src/js/characters/Swordsman.js
+++ src/js/characters/Swordsman.js
@@ -1,7 +1,7 @@
 import Character from "../Character";
 
 export default class Swordsman extends Character {
-  constructor(name, type = "Swordsman", attack = 4000, defence = 1000) {
+  constructor(name, type = "Swordsman", attack = 40, defence = 10) {
     super(name, type);
     this.attack = attack;
     this.defence = defence;
