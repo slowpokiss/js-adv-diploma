a372ed02ccf68cb8ee787163ea2478bf2b565b7e
Michel <hltvexe@mail.ru>
2023-12-09 16:46:28 +0300

    fixed tests

--- src/__tests__/walking.test.js
+++ src/__tests__/walking.test.js
@@ -2,12 +2,12 @@ import { getPossibleArea } from "../js/utils";
 
 test.each([
   ["Magician", 33, [24, 32, 40, 25, 41, 26, 34, 42]],
-  ["Bowman", 33, [24, 32, 40, 25, 41, 26, 34, 42, 19, 51]],
+  ["Bowman", 33, [24, 32, 40, 17, 25, 41, 49, 26, 34, 42, 19, 35, 51]],
   [
     "Swordsman",
     27,
-    [18, 26, 34, 19, 35, 20, 28, 36, 9, 41, 13, 45, 0, 48, 6, 54, 63],
+    [31, 0, 24, 48, 9, 25, 41, 18, 26, 34, 3, 11, 19, 35, 43, 51, 59, 20, 28, 36, 13, 29, 45, 6, 30, 54, 63]
   ],
-])("calculating position (%i, %i)", (characterType, index, expected) => {
+])("calculating area for %s index - %i", (characterType, index, expected) => {
   expect(getPossibleArea(characterType, index)).toEqual(expected);
 });
