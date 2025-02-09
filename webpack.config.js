2bf69e673b62a788cc7ed386c41a97bc65234c09
Michel <hltvexe@mail.ru>
2025-02-10 02:38:57 +0300

    config files changes for gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
-
 const path = require('node:path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: './',
   },
   module: {
     rules: [
@@ -34,13 +37,13 @@ module.exports = {
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
-      }
+      },
     ],
   },
   plugins: [
     new HtmlWebPackPlugin({
       template: './src/index.html',
       filename: './index.html',
-    })
+    }),
   ],
 };
